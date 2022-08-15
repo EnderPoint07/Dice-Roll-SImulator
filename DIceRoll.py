ef48ec6cd686ed0242e633003c7b355ad1628054
EnderPoint07 <anshhd64@hotmail.com>
2022-08-15 12:39:16 +0530

    Make it work

--- DIceRoll.py
+++ DIceRoll.py
@@ -1,13 +1,16 @@
 # This program emulates a dice roll
 import random
 
+
 def roll(output):
     output = random.randrange(1, 7)
     return output
 
+
 start = input("Enter r to roll the dice\n")
 while start == "r":
     result = 0
-    print(roll(result), '\n ----------------------------------------------------------------')
-    start = input("Enter r to continue rolling or enter anything else to quit\n")
-
+    print(roll(result),
+          '\n ----------------------------------------------------------------')
+    start = input(
+        "Enter r to continue rolling or enter anything else to quit\n")
